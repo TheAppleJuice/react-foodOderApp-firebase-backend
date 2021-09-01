80e00583e6b1a46d145212d9799514720f59fb07
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-09-01 12:42:31 +0200

    moved dummy meals to db

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,7 @@ import classes from './AvailableMeals.module.css';
 
 
 const AvailableMeals = () => {
+    fetch()
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
